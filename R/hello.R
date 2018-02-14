27cb6b735ca5859ec919feaf090ad5d22a25b4ff
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2018-02-14 19:32:46 +0100

    ascii art is fun

--- R/hello.R
+++ R/hello.R
@@ -1,12 +1,17 @@
-# Hello, world!
+###############################################################################
+#                 _          _                     
+#                | |        | |                    
+#                | |__   ___| |_ __   ___ _ __ ___ 
+#                | '_ \ / _ \ | '_ \ / _ \ '__/ __|
+#                | | | |  __/ | |_) |  __/ |  \__ \
+#                |_| |_|\___|_| .__/ \___|_|  |___/
+#                              | |                  
+#                              |_|                  
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
+###############################################################################
+
+
+
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Build and Reload Package:  'Ctrl + Shift + B'
