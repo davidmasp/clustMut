4868da80324bcdc5df246a2d1f96e999b43d2d74
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2018-05-28 19:20:07 +0200

    adding function to detect pairs (see issue #8 )

---
+++ R/pairs.R
@@ -0,0 +1,11 @@
+
+
+find_pairs_VR <- function(vr,win_length){
+  dat_vr = vr
+  win = win_length
+  dat_ext = genomicHelpersDMP::extend(dat_vr,upstream = win/2,downstream = win/2)
+  ovr = findOverlaps(dat_ext,dat_vr)
+  pairs = ovr[!duplicated(t(apply(as.matrix(ovr), 1, sort))),] 
+  pairs = pairs[queryHits(pairs) != subjectHits(pairs)]
+  return(pairs)
+}
