4f4309196a7ca9ffdf152121967c06a3e9af3718
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2018-06-20 10:27:40 +0200

    minor changes

--- R/pairs.R
+++ R/pairs.R
@@ -5,7 +5,7 @@ find_pairs_VR <- function(vr,win_length){
   win = win_length
   dat_ext = genomicHelpersDMP::extend(dat_vr,upstream = win/2,downstream = win/2)
   ovr = findOverlaps(dat_ext,dat_vr)
-  pairs = ovr[!duplicated(t(apply(as.matrix(ovr), 1, sort))),] 
+  pairs = ovr[!duplicated(t(apply(as.matrix(ovr), 1, sort))),]
   pairs = pairs[queryHits(pairs) != subjectHits(pairs)]
   return(pairs)
 }
