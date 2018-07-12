059d5ba1e3677ca04109333aaf98a8f10ef1dc31
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2018-07-12 11:42:42 +0200

    solving bug #31

--- R/pairs.R
+++ R/pairs.R
@@ -4,8 +4,8 @@ find_pairs_VR <- function(vr,win_length){
   dat_vr = vr
   win = win_length
   dat_ext = genomicHelpersDMP::extend(dat_vr,upstream = win/2,downstream = win/2)
-  ovr = findOverlaps(dat_ext,dat_vr)
+  ovr = GenomicRanges::findOverlaps(dat_ext,dat_vr)
   pairs = ovr[!duplicated(t(apply(as.matrix(ovr), 1, sort))),]
-  pairs = pairs[queryHits(pairs) != subjectHits(pairs)]
+  pairs = pairs[S4Vectors::queryHits(pairs) != S4Vectors::subjectHits(pairs)]
   return(pairs)
 }
