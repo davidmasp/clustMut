2dfba372e5c960be40fec49a6e05ffcc61b4d5ac
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2018-09-26 17:32:05 +0200

    new handeling error messages

--- tests_exec/run_tests.sh
+++ tests_exec/run_tests.sh
@@ -12,7 +12,7 @@ if hash sbatch 2>/dev/null; then
     sbatch run_test_sFDR.sh
 
     srun --jobname "test_check" Rscript check_output_status.R
-
+    EXIT_STATUS=$?
 else
     echo "Running exec test of clustmut in local mode"
 
@@ -21,6 +21,17 @@ else
     sh run_test_FDR.sh
     
     srun --jobname "test_check" Rscript check_output_status.R
+    EXIT_STATUS=$?
 fi
 
 
+
+if [ $EXIT_STATUS -ne 0 ]; then
+    echo "Error in tests"
+
+    # ideally send email here
+else
+    echo "All tests passed"
+
+    # ideally send email here
+fi
