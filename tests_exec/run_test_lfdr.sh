3a77f43d49b30018fc6cd4ca1f45015be4aa2478
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2019-06-19 15:44:35 +0200

    this is never ending

--- tests_exec/run_test_lfdr.sh
+++ tests_exec/run_test_lfdr.sh
@@ -16,7 +16,7 @@
 srun Rscript ../exec/clustmut_distance.R -i data/ \
                 --glob "*TCGA-CG-5723-01A-11D-1600-08_randomized_FN_0.75_simulated_clust_rmdup.tsv_w500000.randomized.tsv" \
                 --recursive \
-                -b "tests_exec/data/TCGA-CG-5723-01A-11D-1600-08_mutations_strand2.txt" \
+                -b "data/TCGA-CG-5723-01A-11D-1600-08_mutations_strand2.txt" \
                 -o mela_au_fdr \
                 -N 1 \
                 -Vvu
