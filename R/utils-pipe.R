000373c102728358f90ca75e524e9eda85f34ced
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2019-06-19 10:21:06 +0200

    adding new utils cleaning

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -9,3 +9,28 @@
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
+
+
+
+ascii_message <- function(version = packageVersion("clustMut")) {
+
+nchar_version = nchar(as.character(version))
+
+nchar_rest = 46 - 12 - nchar_version -1
+rest_string = paste(rep(' ',nchar_rest),collapse = "")
+vector = c("+--------------------------------------------+",
+  "|                                            |",
+  glue::glue("|  ClustMut {version}{rest_string}|"),
+  "|                                            |",
+  "|          +  +  +         +          +      |",
+  "|          |  |  |         |          |      |",
+  "|   -------+--+--+---------+----------+---   |",
+  "|                                            |",
+  "+--------------------------------------------+")
+
+  for (i in vector){
+    print(i)
+  }
+
+
+}
