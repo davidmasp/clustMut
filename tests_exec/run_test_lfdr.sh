450a6585993d94f49862fa414c83bce5f02a5651
IRBBARCELONA\dmas <david.mas@irbbarcelona.org>
2019-06-19 15:09:51 +0200

    temporally removing runing tests

--- tests_exec/run_test_lfdr.sh
+++ tests_exec/run_test_lfdr.sh
@@ -12,9 +12,18 @@
 
 #SBATCH --mem=10000
 
-Rscript ../exec/clustmut_distance.R -i data/ \
-                --glob "*.tsv" \
+
+srun Rscript ../exec/clustmut_distance.R -i data/ \
+                --glob "*TCGA-CG-5723-01A-11D-1600-08_randomized_FN_0.75_simulated_clust_rmdup.tsv_w500000.randomized.tsv" \
                 --recursive \
+                -b "tests_exec/data/TCGA-CG-5723-01A-11D-1600-08_mutations_strand2.txt" \
                 -o mela_au_fdr \
                 -N 1 \
-                -Vvu 
+                -Vvu
+#
+# srun Rscript ../exec/clustmut_distance.R -i data/ \
+#                 --glob "*.tsv" \
+#                 --recursive \
+#                 -o mela_au_fdr \
+#                 -N 1 \
+#                 -Vvu
